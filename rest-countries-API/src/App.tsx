c41dba28657421475a8e5b3cf046bf1a20b0c689
Frederic Soc <tocomorales30@gmail.com>
2025-01-13 15:50:52 -0600

    instaling bootstrap

--- rest-countries-API/src/App.tsx
+++ rest-countries-API/src/App.tsx
@@ -1,11 +1,10 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
-    <h1>Hola bro</h1>
+    <h1>ssdfdsf fsdfds</h1>
   )
 }
 
