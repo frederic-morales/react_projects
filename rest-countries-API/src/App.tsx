410b33597589d20e3bfec8619efa4191ecdc7eee
Frederic Morales <tocomorales30@gmail.com>
2025-01-13 21:58:06 -0600

    Adding tailwindcss

--- rest-countries-API/src/App.tsx
+++ rest-countries-API/src/App.tsx
@@ -1,11 +1,13 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import Header from "./components/header";
 
 function App() {
   return (
-    <h1>ssdfdsf fsdfds</h1>
-  )
+    <>
+      <Header></Header>
+      <h1 className="text-7xl">Soy otro h1</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ rest-countries-API/src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <>
+      <h1 className="bg-black border-l">Where in the world</h1>
+      <div>
+        <img src="" alt="" />
+        <p className="text-1xl font-bold underline">Dark Mode</p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
