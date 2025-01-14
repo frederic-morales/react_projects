5449e2237497f0a3b1f0a0332228500f59334ffd
Frederic Soc <tocomorales30@gmail.com>
2025-01-14 12:27:14 -0600

    Instaling and configurating tailwindcss

--- rest-countries-API/src/App.tsx
+++ rest-countries-API/src/App.tsx
@@ -1,13 +1,14 @@
-import "./App.css";
-import Header from "./components/header";
+// import { useState } from 'react'
+import './App.css'
 
 function App() {
+  // const [count, setCount] = useState(0)
+
   return (
-    <>
-      <Header></Header>
-      <h1 className="text-7xl">Soy otro h1</h1>
-    </>
-  );
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
 
-export default App;
+export default App
--- rest-countries-API/src/components/Header.tsx
+++ rest-countries-API/src/components/Header.tsx
@@ -1,13 +1,12 @@
-const Header = () => {
-  return (
-    <>
-      <h1 className="bg-black border-l">Where in the world</h1>
-      <div>
-        <img src="" alt="" />
-        <p className="text-1xl font-bold underline">Dark Mode</p>
-      </div>
-    </>
-  );
-};
+type Props = {
+    title: string
+}
 
-export default Header;
+function Header(props: Props) {
+    const { title } = props 
+    return (
+        <div>{title}</div>
+    )
+}
+
+export default Header
