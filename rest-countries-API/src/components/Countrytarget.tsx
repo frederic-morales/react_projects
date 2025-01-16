1609720061f39d5b51e8ca932f0e9c75bf4c81d0
Frederic Soc <tocomorales30@gmail.com>
2025-01-16 11:33:01 -0600

    Creating the Countrytarget component

---
+++ rest-countries-API/src/components/Countrytarget.tsx
@@ -0,0 +1,21 @@
+// type Props = {};
+
+function Countrytarget() {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <ul>
+          <li>Localidad</li>
+          <li>Poblacion</li>
+          <li>Tamaño</li>
+          <li>Bandera</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Countrytarget;
