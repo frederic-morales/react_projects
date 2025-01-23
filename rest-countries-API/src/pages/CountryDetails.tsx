85aa19ddf782d89e9d55c7d56b229ae325a30cc2
Frederic Soc <tocomorales30@gmail.com>
2025-01-23 16:00:02 -0600

    Styling the CountryDetails component part 3

--- rest-countries-API/src/pages/CountryDetails.tsx
+++ rest-countries-API/src/pages/CountryDetails.tsx
@@ -27,15 +27,16 @@ function CountryDetails() {
 
   return (
     <div className="flex items-center justify-center w-full dark:bg-veryDarkBlueBG">
-      <div className="w-11/12 mt-7">
+      <div className="w-10/12 mt-7 max-w-xs">
         <button className="py-1 px-8 bg-darkBlue shadow-lg mb-6">Back</button>
-        <div className="w-full">
-          <img
-            src={country.flags?.png}
-            alt=""
-            className="w-full h-full object-fill"
-          />
-          <div></div>
+        <div className="h-96 w-full rounded-lg flex flex-col justify-between">
+          <div className="w-full h-56">
+            <img
+              src={country.flags?.png}
+              alt=""
+              className="w-full h-full object-fill"
+            />
+          </div>
           <p>{country.name?.common}</p>
           <ul>
             <li>
